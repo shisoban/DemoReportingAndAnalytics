f8ac5bf4ef1324eb2b92d98712e3cfe87412dc6a
Hisoban <hisoban3@gmail.com>
2016-12-16 00:05:28 +0530

    first commit

---
+++ src/com/demoReportingAndAnalytics/LoginAction.java
@@ -0,0 +1,33 @@
+package com.demoReportingAndAnalytics;
+
+public class LoginAction {
+	
+	private String name,password;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String execute(){
+		if(name.equals("admin") && password.equals("admin")){
+			return "success";
+		}
+		else{
+			return "error";
+		}
+	
+	}
+
+}
